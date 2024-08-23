e3904d5ff6929a2f107338353f74fb164470b361
CrawlerCode <41094392+CrawlerCode@users.noreply.github.com>
2024-08-23 19:31:21 +0200

    feat: Create project from template

---
+++ dev/plugin.spec.ts
@@ -0,0 +1,30 @@
+import type { Server } from 'http'
+import mongoose from 'mongoose'
+import payload from 'payload'
+import { start } from './src/server'
+
+describe('Plugin tests', () => {
+  let server: Server
+
+  beforeAll(async () => {
+    await start({ local: true })
+  })
+
+  afterAll(async () => {
+    await mongoose.connection.dropDatabase()
+    await mongoose.connection.close()
+    server.close()
+  })
+
+  // Add tests to ensure that the plugin works as expected
+
+  // Example test to check for seeded data
+  it('seeds data accordingly', async () => {
+    const newCollectionQuery = await payload.find({
+      collection: 'new-collection',
+      sort: 'createdAt',
+    })
+
+    expect(newCollectionQuery.totalDocs).toEqual(1)
+  })
+})
---
+++ dev/src/payload.config.ts
@@ -0,0 +1,44 @@
+import { buildConfig } from 'payload/config';
+import path from 'path';
+import Users from './collections/Users';
+import Examples from './collections/Examples';
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { slateEditor } from '@payloadcms/richtext-slate'
+import { samplePlugin } from '../../src/index'
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    bundler: webpackBundler(),
+    webpack: config => {
+      const newConfig = {
+        ...config,
+        resolve: {
+          ...config.resolve,
+          alias: {
+            ...(config?.resolve?.alias || {}),
+            react: path.join(__dirname, '../node_modules/react'),
+            'react-dom': path.join(__dirname, '../node_modules/react-dom'),
+            payload: path.join(__dirname, '../node_modules/payload'),
+          },
+        },
+      }
+      return newConfig
+    },
+  },
+  editor: slateEditor({}),
+  collections: [
+    Examples, Users,
+  ],
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+  },
+  plugins: [samplePlugin({ enabled: true })],
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI,
+  }),
+})
