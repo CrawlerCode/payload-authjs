7d0ec8d7238aa7213e775bf8ea5610ebb51cf800
CrawlerCode <joshua.jordan@outlook.de>
2024-10-30 12:12:14 +0100

    chore: Migrate to @payloadcms/eslint-config 3 beta

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+// @ts-check
+
+import payloadEsLintConfig from "@payloadcms/eslint-config";
+
+export const defaultESLintIgnores = [
+  "**/.temp",
+  "**/.*", // ignore all dotfiles
+  "**/.git",
+  "**/next-env.d.ts",
+  "**/jest.config.js",
+  "**/eslint.config.js",
+  "**/payload-types.ts",
+  "dev/src/app/(payload)/",
+  "**/dist/",
+  "**/build/",
+  "**/node_modules/",
+  "**/temp/",
+];
+
+export default [
+  ...payloadEsLintConfig,
+  {
+    // Modify any rules from payload's config that you want to override/disable
+    rules: {
+      "no-restricted-exports": "off",
+      "perfectionist/sort-union-types": "off",
+      "perfectionist/sort-objects": "off",
+      "perfectionist/sort-interfaces": "off",
+      "perfectionist/sort-object-types": "off",
+      "perfectionist/sort-imports": "off",
+      "perfectionist/sort-jsx-props": "off",
+    },
+  },
+  // TODO: Bring in '@payloadcms/eslint-plugin' for 'payload/proper-payload-logger-usage' rule
+  {
+    ignores: defaultESLintIgnores,
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: "latest",
+        projectService: {
+          maximumDefaultProjectFileMatchCount_THIS_WILL_SLOW_DOWN_LINTING: 40,
+          allowDefaultProject: ["scripts/*.ts", "*.js", "*.mjs", "*.spec.ts", "*.d.ts"],
+        },
+        // projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+];
