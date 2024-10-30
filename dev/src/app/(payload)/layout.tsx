e4a62824c3934b26efa39209d7af0b33e51a9380
CrawlerCode <joshua.jordan@outlook.de>
2024-10-30 16:08:26 +0100

    chore(dev): Update payload files

--- dev/src/app/(payload)/layout.tsx
+++ dev/src/app/(payload)/layout.tsx
@@ -1,21 +1,21 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-import configPromise from "@payload-config";
-import "@payloadcms/next/css";
-import { RootLayout } from "@payloadcms/next/layouts";
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-import React from "react";
+import configPromise from '@payload-config'
+import '@payloadcms/next/css'
+import { RootLayout } from '@payloadcms/next/layouts'
+import React from 'react'
 
-import { importMap } from "./admin/importMap";
-import "./custom.scss";
+import './custom.scss'
+import { importMap } from './admin/importMap'
 
 type Args = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
 const Layout = ({ children }: Args) => (
   <RootLayout importMap={importMap} config={configPromise}>
     {children}
   </RootLayout>
-);
+)
 
-export default Layout;
+export default Layout
