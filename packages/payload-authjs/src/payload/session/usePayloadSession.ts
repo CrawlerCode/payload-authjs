b4d06af561450fa0e8611b4f37c6467f206869b5
CrawlerCode <41094392+CrawlerCode@users.noreply.github.com>
2025-04-06 22:51:58 +0200

    refactor(usePayloadSession): Remove unnecessary null check

--- packages/payload-authjs/src/payload/session/usePayloadSession.ts
+++ packages/payload-authjs/src/payload/session/usePayloadSession.ts
@@ -8,11 +8,5 @@ import { Context, type SessionContext } from "./PayloadSessionProvider";
  * Client side hook to retrieve the session from the context provider (PayloadSessionProvider)
  */
 export const usePayloadSession = <TSlug extends CollectionSlug = "users">() => {
-  const result = use<SessionContext<TSlug>>(Context);
-
-  if (!result) {
-    throw new Error("usePayloadSession must be used within a PayloadSessionProvider");
-  }
-
-  return result;
+  return use<SessionContext<TSlug>>(Context);
 };
